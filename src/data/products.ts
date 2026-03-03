afe66fbc6b558f45512fb618e25f9d2f3a22b06b
Gonzalo Ortiz <98592982+GonzaaOrtiz16@users.noreply.github.com>
2026-03-03 16:38:20 -0300

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -20,12 +20,12 @@ export interface Product {
 
 // Categorías para Rafaghelli Motos
 export const categories = [
-  { id: "Transmisión", name: "Transmisión", icon: "wrench", image: "https://unsplash.com/es/fotos/un-primer-plano-de-un-reloj-9ehmPYBJkyU" },
-  { id: "Frenos", name: "Frenos", icon: "disc", image: "https://unsplash.com/es/fotos/primer-plano-del-sistema-de-frenos-delanteros-de-una-motocicleta-sKGyaOusfLQ" },
-  { id: "Motor", name: "Motor", icon: "settings", image: "https://unsplash.com/es/fotos/primer-plano-de-las-manos-de-la-mujer-del-tecnico-del-vehiculo-en-los-guantes-de-trabajo-que-fijan-las-piezas-metalicas-del-automovil-del-acero-inoxidable-rmVrCYNZ1Qk" },
-  { id: "Cubiertas y Cámaras", name: "Cubiertas", icon: "circle", image: "https://unsplash.com/es/fotos/una-motocicleta-estacionada-en-la-cima-de-un-exuberante-campo-verde-9PGdccvTfjc" },
-  { id: "Aceites y Filtros", name: "Aceites", icon: "droplets", image: "https://unsplash.com/es/fotos/la-botella-de-refrigerante-se-coloca-en-una-motocicleta-61VqlOr9-Ss" },
-  { id: "Accesorios y Cascos", name: "Cascos", icon: "hard-hat", image: "https://unsplash.com/es/fotos/persona-con-casco-negro-y-chaqueta-azul-DgwCCZeVrG4" },
+  { id: "Transmisión", name: "Transmisión", icon: "wrench", image: "https://images.unsplash.com/photo-1772566385505-cfccf766b5f0?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { id: "Frenos", name: "Frenos", icon: "disc", image: "https://images.unsplash.com/photo-1772566386564-017cfeb14ce0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { id: "Motor", name: "Motor", icon: "settings", image: "https://img.freepik.com/foto-gratis/vista-cercana-piezas-motocicleta_23-2150704627.jpg" },
+  { id: "Cubiertas y Cámaras", name: "Cubiertas", icon: "circle", image: "https://images.unsplash.com/photo-1772566387970-40f3eaf44884?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { id: "Aceites y Filtros", name: "Aceites", icon: "droplets", image: "https://images.unsplash.com/photo-1772566384197-330880cb04ac?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { id: "Accesorios y Cascos", name: "Cascos", icon: "hard-hat", image: "https://images.unsplash.com/photo-1772566388989-1a7f1dc33b3a?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
 ];
 
 export const brands = ["Honda", "Yamaha", "Suzuki", "Bajaj", "LS2", "Motul", "Pirelli"];
